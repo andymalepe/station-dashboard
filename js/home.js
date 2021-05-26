5f83422053aa5548fd130510cde441bc7a48b703
Kagiso Malepe <kagiso@cagegroup.co.za>
2021-05-26 13:15:25 +0200

    Set current forecast

--- js/home.js
+++ js/home.js
@@ -1,11 +1,47 @@
 // YR LINK: https://api.met.no/weatherapi/locationforecast/2.0/complete?lat=-71.6703&lon=-2.8377&altitude=850
 $(document).ready(function(){
+  const lat = '-71.6703';
+  const lon = '-2.8377';
+  const altitude = '850';
+  const weatherLocation = 'SANAE IV, Antarctica';
+
+//   var dt = new Date();
+//   dt.setHours( dt.getHours() + 2 );    
+//   // document.write( dt );
+//   console.log(dt);
+//   if ('caches' in window) {
+//     //const newCache = await caches.open('new-cache');
+//   }
+
   $.ajax({
-    url: 'https://api.met.no/weatherapi/locationforecast/2.0/complete?lat=-71.6703&lon=-2.8377&altitude=850',
+    url: 'https://api.met.no/weatherapi/locationforecast/2.0/compact?lat='+lat+'&lon='+lon+'&altitude='+altitude,
     type: 'GET',
     dataType: 'json',
     success: function(data){
-      console.log(data);
+      const days = ['Sunday','Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+      let current_dt = new Date(data.properties.meta.updated_at);
+      let tomorrow_dt = new Date(current_dt);
+      tomorrow_dt.setDate(tomorrow_dt.getDate() + 1);
+      tomorrow_dt.setHours(12,0,0,0);
+      let timeseries_dt = new Date(data.properties.timeseries[0].time);
+
+      console.log(tomorrow_dt);
+      //set current weather forecast
+      $('#dayOfWeek').text(days[timeseries_dt.getDay()]);
+      $('.weather-date').text(data.properties.timeseries[0].time);
+      $('#air-temperature').text(data.properties.timeseries[0].data.instant.details.air_temperature);
+      //loop through time series for future firecasts
+      $.each(data.properties.timeseries, function(index, timeseries){
+        let datetime = new Date(timeseries.time);
+        //get tomorrow 12h time
+          if (tomorrow_dt.getDate() === datetime.getDate()) {
+            console.log(tomorrow_dt);
+            console.log(datetime);
+            console.log('dates are equal');
+          }else {
+            console.log('not equal');
+          }
+      });
     },
     error: function(e){
 
--- pages/home.php
+++ pages/home.php
@@ -17,43 +17,43 @@
                     <div class="card card-weather">
                         <div class="card-body">
                             <div class="weather-date-location">
-                                <h3>Friday</h3>
-                                <p class="text-gray"> <span class="weather-date">25 March, 2019</span> <span class="weather-location">Sydney, Australia</span> </p>
+                                <h3 id="dayOfWeek">Friday</h3>
+                                <p class="text-gray"> <span class="weather-date">25 March, 2019</span> <span class="weather-location">SANAE IV, Antarctica</span> </p>
                             </div>
                             <div class="weather-data d-flex">
                                 <div class="mr-auto">
-                                    <h4 class="display-3">32 <span class="symbol">°</span>C</h4>
+                                    <h4 class="display-3" id="air-temperature">32 <span class="symbol">°</span>C</h4>
                                     <p> Cloudy </p>
                                 </div>
                             </div>
                         </div>
                         <div class="card-body p-0">
-                            <div class="d-flex weakly-weather">
-                                <div class="weakly-weather-item">
+                            <div class="d-flex weekly-weather">
+                                <div class="weekly-weather-item">
                                     <p class="mb-0"> Sun </p> <i class="mdi mdi-weather-cloudy"></i>
                                     <p class="mb-0"> 30° </p>
                                 </div>
-                                <div class="weakly-weather-item">
+                                <div class="weekly-weather-item">
                                     <p class="mb-1"> Mon </p> <i class="mdi mdi-weather-hail"></i>
                                     <p class="mb-0"> 31° </p>
                                 </div>
-                                <div class="weakly-weather-item">
+                                <div class="weekly-weather-item">
                                     <p class="mb-1"> Tue </p> <i class="mdi mdi-weather-partlycloudy"></i>
                                     <p class="mb-0"> 28° </p>
                                 </div>
-                                <div class="weakly-weather-item">
+                                <div class="weekly-weather-item">
                                     <p class="mb-1"> Wed </p> <i class="mdi mdi-weather-pouring"></i>
                                     <p class="mb-0"> 30° </p>
                                 </div>
-                                <div class="weakly-weather-item">
+                                <div class="weekly-weather-item">
                                     <p class="mb-1"> Thu </p> <i class="mdi mdi-weather-pouring"></i>
                                     <p class="mb-0"> 29° </p>
                                 </div>
-                                <div class="weakly-weather-item">
+                                <div class="weekly-weather-item">
                                     <p class="mb-1"> Fri </p> <i class="mdi mdi-weather-snowy-rainy"></i>
                                     <p class="mb-0"> 31° </p>
                                 </div>
-                                <div class="weakly-weather-item">
+                                <div class="weekly-weather-item">
                                     <p class="mb-1"> Sat </p> <i class="mdi mdi-weather-snowy"></i>
                                     <p class="mb-0"> 32° </p>
                                 </div>
@@ -67,7 +67,7 @@
     </div>
 
     <?php require_once('../includes/scripts.php'); ?>
-    <script type="text/javascript" src="pages/home.js">
+    <script type="text/javascript" src="../js/home.js">
 
     </script>
   </body>
